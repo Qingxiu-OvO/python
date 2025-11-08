77243b80936ca98e3484c18e7b4a573b409d3e89
Qingxiu <2247405312@qq.com>
2025-11-08 23:13:25 +0800

    写了一些没素质的代码

---
+++ 类型运算.py
@@ -0,0 +1,13 @@
+x='fuck '  
+y='you'
+z=x+y
+z=z+' 1'
+print(z)
+name=input("what's your name?")
+print('Welcome',name)
+b=input('1+1=?')
+b=int(b)
+if b==2:
+    print('Yes')
+else:
+    print('SB')
